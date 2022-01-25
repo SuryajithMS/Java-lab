8ede6ed1d773114896b9657a7b0928d4926e19d0
SuryajithMS <76899256+SuryajithMS@users.noreply.github.com>
2022-01-25 20:05:49 +0530

    Create sum.java

---
+++ cycle 1/sum.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		int a,b,sum=0;
+		System.out.println("enter first number");
+		a= sc.nextInt();
+		System.out.println("enter second number");
+		b= sc.nextInt();
+		sum=a+b;
+		System.out.println("sum is :" + sum);
+	}
+}
