5d8ba800e8cd41a8af0fd76a371ca1ba7b52ccbc
SuryajithMS <76899256+SuryajithMS@users.noreply.github.com>
2022-01-25 20:10:36 +0530

    Create Evenodd.java

---
+++ cycle 1/Evenodd.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		int a;
+		System.out.println("enter a number");
+		a = sc.nextInt();
+		if(a%2==0)
+		{
+		    System.out.println("Entered number is even");
+		}
+		else{
+		    System.out.println("Entered number is odd");
+		}
+		
+	}
+}
